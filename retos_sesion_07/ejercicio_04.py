6db48301372555d75732aadb6861245c70277e9a
Scarleth Jimenez <scarnat1993@gmail.com>
2024-05-21 11:08:50 -0400

    Invertir la palabra programacion

---
+++ retos_sesion_07/ejercicio_04.py
@@ -0,0 +1,7 @@
+def invertir_palabra():
+    palabra = "programación"
+    palabra_invertida = palabra[::-1]
+    print("Palabra invertida:", palabra_invertida)
+
+if __name__ == "__main__":
+    invertir_palabra()
