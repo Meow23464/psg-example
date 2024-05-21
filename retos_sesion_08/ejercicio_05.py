a38cec9cc6794540c9e260c4e2b9c94d540cf6bd
Scarleth Jimenez <scarnat1993@gmail.com>
2024-05-21 17:09:17 -0400

    Tupla de numeros telefonicos

---
+++ retos_sesion_08/ejercicio_05.py
@@ -0,0 +1,7 @@
+guia_telefonica = (
+    ("Juan", "591123456", "La Paz"),
+    ("Pedro", "591546324", "La Paz"),
+    ("Maria", "591222421", "El Alto"),
+    ("Sol", "591753159", "Oruro")
+)
+print("Guía telefónica:", guia_telefonica)
