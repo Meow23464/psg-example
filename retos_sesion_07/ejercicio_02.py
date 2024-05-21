36e5b1379b33216008a124e6f449fef1022bc445
Scarleth Jimenez <scarnat1993@gmail.com>
2024-05-21 11:02:52 -0400

    Separar la palabra fluorescente

---
+++ retos_sesion_07/ejercicio_02.py
@@ -0,0 +1,8 @@
+def sumar_enteros():
+    num1 = int(input("Ingresa el primer número: "))
+    num2 = int(input("Ingresa el segundo número: "))
+    suma = num1 + num2
+    print("La suma es:", suma)
+
+if __name__ == "__main__":
+    sumar_enteros()
