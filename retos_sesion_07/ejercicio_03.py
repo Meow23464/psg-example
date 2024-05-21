80fbc53652d021a73a45329a3728f0dcec737a2c
Scarleth Jimenez <scarnat1993@gmail.com>
2024-05-21 11:04:20 -0400

    Mostrar los caracteres con indice par de la palabra fluorescente

---
+++ retos_sesion_07/ejercicio_03.py
@@ -0,0 +1,7 @@
+def caracteres_pares():
+    palabra = "Fluorescente"
+    caracteres_pares = palabra[::2]
+    print("Caracteres con índice par:", caracteres_pares)
+
+if __name__ == "__main__":
+    caracteres_pares()
