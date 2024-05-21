a40c247357fb97e8f14a43f712aa5953cd6f111e
Scarleth Jimenez <scarnat1993@gmail.com>
2024-05-21 18:13:10 -0400

    Menus de restaurantes

---
+++ retos_sesion_10/ejercicio_02.py
@@ -0,0 +1,6 @@
+menu_mexicano = {"Tacos", "Enchiladas", "Guacamole", "Tamales"}
+menu_italiano = {"Pizza", "Pasta", "Lasagna", "Tiramisú"}
+
+# Fusionar ambos menús
+menu_fusion = menu_mexicano | menu_italiano
+print("Menú de comida fusión:", menu_fusion)
