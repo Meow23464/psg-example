43830d3d59413db2536f3b445c80f3ad10a95f10
Scarleth Jimenez <scarnat1993@gmail.com>
2024-06-05 15:02:14 -0400

    Funcion que recibe cadena y la invierte

---
+++ retos_sesion_14/ejercicio_05.py
@@ -0,0 +1,6 @@
+def invertir_cadena(cadena):
+    return cadena[::-1]
+
+cadena = input("Ingrese una cadena: ")
+cadena_invertida = invertir_cadena(cadena)
+print(f"La cadena invertida es: {cadena_invertida}")
